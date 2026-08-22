cceb53a405df0f7d859baa5f791d0f426685d8e6
𝐀ʀᴄʜᴏɴ [🇮🇳] <teamoctavex@gmail.com>
2026-08-22 19:54:06 +0530

    Update _inline.py

--- AloneX/helpers/_inline.py
+++ AloneX/helpers/_inline.py
@@ -1239,7 +1239,7 @@ class Inline:
 
                     self.ikb(
                         text="Tʜᴇ Aʀᴄʜᴏɴ",
-                        user_id=config.OWNER_USERNAME,
+                        user_id=config.OWNER_ID,
                         style=ButtonStyle.DANGER,
                         emoji_key="owner",
                     ),
